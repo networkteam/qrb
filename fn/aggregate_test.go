532989dfecf4f279ca5383ca16f220673e08b3fe
Christopher Hlubek <christopher.hlubek@networkteam.com>
2023-05-11 16:04:56 +0200

    Add test cases for aggregate functions

--- fn/aggregate_test.go
+++ fn/aggregate_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/networkteam/qrb"
+	"github.com/networkteam/qrb/builder"
 	"github.com/networkteam/qrb/fn"
 	"github.com/networkteam/qrb/internal/testhelper"
 )
@@ -68,5 +69,151 @@ func TestAggregateExpressions(t *testing.T) {
 		GROUP BY "Title"
 		HAVING count(DISTINCT "Genre"."Name") > 1
 		`, sql)
+	})
+
+	t.Run("functions", func(t *testing.T) {
+		tt := []struct {
+			name        string
+			fn          builder.Exp
+			expectedSQL string
+		}{
+			{
+				name:        "array_agg",
+				fn:          fn.ArrayAgg(qrb.N("title")).OrderBy(qrb.N("title")).Desc(),
+				expectedSQL: "array_agg(title ORDER BY title DESC)",
+			},
+			{
+				name:        "avg",
+				fn:          fn.Avg(qrb.N("price")),
+				expectedSQL: "avg(price)",
+			},
+			{
+				name:        "bit_and",
+				fn:          fn.BitAnd(qrb.N("flags")),
+				expectedSQL: "bit_and(flags)",
+			},
+			{
+				name:        "bit_or",
+				fn:          fn.BitOr(qrb.N("flags")),
+				expectedSQL: "bit_or(flags)",
+			},
+			{
+				name:        "bit_xor",
+				fn:          fn.BitXor(qrb.N("flags")),
+				expectedSQL: "bit_xor(flags)",
+			},
+			{
+				name:        "bool_and",
+				fn:          fn.BoolAnd(qrb.N("active")),
+				expectedSQL: "bool_and(active)",
+			},
+			{
+				name:        "bool_or",
+				fn:          fn.BoolOr(qrb.N("active")),
+				expectedSQL: "bool_or(active)",
+			},
+			{
+				name:        "count",
+				fn:          fn.Count(qrb.N("*")),
+				expectedSQL: "count(*)",
+			},
+			{
+				name:        "json_agg",
+				fn:          fn.JsonAgg(qrb.N("title")),
+				expectedSQL: `json_agg(title)`,
+			},
+			{
+				name:        "jsonb_agg",
+				fn:          fn.JsonbAgg(qrb.N("title")),
+				expectedSQL: `jsonb_agg(title)`,
+			},
+			{
+				name:        "json_object_agg",
+				fn:          fn.JsonObjectAgg(qrb.N("title"), qrb.N("price")),
+				expectedSQL: `json_object_agg(title, price)`,
+			},
+			{
+				name:        "string_agg",
+				fn:          fn.StringAgg(qrb.N("title"), qrb.String(",")).OrderBy(qrb.N("title")).Desc(),
+				expectedSQL: "string_agg(title, ',' ORDER BY title DESC)",
+			},
+			{
+				name:        "max",
+				fn:          fn.Max(qrb.N("price")),
+				expectedSQL: "max(price)",
+			},
+			{
+				name:        "min",
+				fn:          fn.Min(qrb.N("price")),
+				expectedSQL: "min(price)",
+			},
+			{
+				name:        "range_agg",
+				fn:          fn.RangeAgg(qrb.N("price")),
+				expectedSQL: "range_agg(price)",
+			},
+			{
+				name:        "range_intersect_agg",
+				fn:          fn.RangeIntersectAgg(qrb.N("price")),
+				expectedSQL: "range_intersect_agg(price)",
+			},
+			{
+				name:        "sum",
+				fn:          fn.Sum(qrb.N("price")),
+				expectedSQL: "sum(price)",
+			},
+			{
+				name:        "xmlagg",
+				fn:          fn.Xmlagg(qrb.N("title")),
+				expectedSQL: "xmlagg(title)",
+			},
+			{
+				name:        "mode",
+				fn:          fn.Mode().WithinGroup().OrderBy(qrb.N("price")).Asc(),
+				expectedSQL: "mode() WITHIN GROUP (ORDER BY price ASC)",
+			},
+			{
+				name:        "percentile_cont",
+				fn:          fn.PercentileCont(qrb.Float(0.5)).WithinGroup().OrderBy(qrb.N("price")).Asc(),
+				expectedSQL: "percentile_cont(0.5) WITHIN GROUP (ORDER BY price ASC)",
+			},
+			{
+				name:        "percentile_disc",
+				fn:          fn.PercentileDisc(qrb.Float(0.5)).WithinGroup().OrderBy(qrb.N("price")).Asc(),
+				expectedSQL: "percentile_disc(0.5) WITHIN GROUP (ORDER BY price ASC)",
+			},
+			{
+				name:        "rank",
+				fn:          fn.Rank().WithinGroup().OrderBy(qrb.N("price")).Asc(),
+				expectedSQL: "rank() WITHIN GROUP (ORDER BY price ASC)",
+			},
+			{
+				name:        "dense_rank",
+				fn:          fn.DenseRank().WithinGroup().OrderBy(qrb.N("price")).Asc(),
+				expectedSQL: "dense_rank() WITHIN GROUP (ORDER BY price ASC)",
+			},
+			{
+				name:        "percent_rank",
+				fn:          fn.PercentRank().WithinGroup().OrderBy(qrb.N("price")).Asc(),
+				expectedSQL: "percent_rank() WITHIN GROUP (ORDER BY price ASC)",
+			},
+			{
+				name:        "cume_dist",
+				fn:          fn.CumeDist().WithinGroup().OrderBy(qrb.N("price")).Asc(),
+				expectedSQL: "cume_dist() WITHIN GROUP (ORDER BY price ASC)",
+			},
+			{
+				name:        "grouping",
+				fn:          fn.Grouping(qrb.N("price"), qrb.N("title")),
+				expectedSQL: "GROUPING(price,title)",
+			},
+		}
+
+		for _, tc := range tt {
+			t.Run(tc.name, func(t *testing.T) {
+				sql, _, _ := qrb.Build(tc.fn).ToSQL()
+				testhelper.AssertSQLEquals(t, tc.expectedSQL, sql)
+			})
+		}
 	})
 }
