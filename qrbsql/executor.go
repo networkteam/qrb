f7271835d24c6e90e43fdb7bfce56e8608547a37
Christopher Hlubek <christopher.hlubek@networkteam.com>
2024-06-05 15:37:24 +0200

    Update comment

--- qrbsql/executor.go
+++ qrbsql/executor.go
@@ -22,7 +22,7 @@ type ExecutorBuilder struct {
 }
 
 // Executor is the interface that wraps the basic Query, QueryRow and Exec methods.
-// It allows to use pgx conn, pool and tx as an executor.
+// It allows to use *sql.DB and *sql.TX as an executor.
 type Executor interface {
 	QueryContext(ctx context.Context, sql string, args ...any) (*sql.Rows, error)
 	QueryRowContext(ctx context.Context, sql string, args ...any) *sql.Row
