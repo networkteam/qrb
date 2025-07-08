9db86578f0a9dee33f56c475b6d5db50845fff9f
Christopher Hlubek <christopher.hlubek@networkteam.com>
2025-07-08 15:30:23 +0200

    feat: implement array functions (#12)

--- fn/array.go
+++ fn/array.go
@@ -11,3 +11,124 @@ import "github.com/networkteam/qrb/builder"
 func Unnest(anyarray builder.Exp, anyarrays ...builder.Exp) builder.FuncBuilder {
 	return builder.Func("unnest", append([]builder.Exp{anyarray}, anyarrays...)...)
 }
+
+// ArrayAppend appends an element to the end of an array.
+//
+//	array_append ( anyarray, anyelement ) → anyarray
+func ArrayAppend(arr builder.Exp, elem builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_append", []builder.Exp{arr, elem})
+}
+
+// ArrayPrepend prepends an element to the beginning of an array.
+//
+//	array_prepend ( anyelement, anyarray ) → anyarray
+func ArrayPrepend(elem builder.Exp, arr builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_prepend", []builder.Exp{elem, arr})
+}
+
+// ArrayCat concatenates two arrays.
+//
+//	array_cat ( anyarray, anyarray ) → anyarray
+func ArrayCat(arr1 builder.Exp, arr2 builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_cat", []builder.Exp{arr1, arr2})
+}
+
+// ArrayDims returns a text representation of an array's dimensions.
+//
+//	array_dims ( anyarray ) → text
+func ArrayDims(arr builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_dims", []builder.Exp{arr})
+}
+
+// ArrayNdims returns the number of dimensions of an array.
+//
+//	array_ndims ( anyarray ) → integer
+func ArrayNdims(arr builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_ndims", []builder.Exp{arr})
+}
+
+// ArrayLength returns the length of the requested array dimension.
+//
+//	array_length ( anyarray, integer ) → integer
+func ArrayLength(arr builder.Exp, dim builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_length", []builder.Exp{arr, dim})
+}
+
+// ArrayLower returns the lower bound of the requested array dimension.
+//
+//	array_lower ( anyarray, integer ) → integer
+func ArrayLower(arr builder.Exp, dim builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_lower", []builder.Exp{arr, dim})
+}
+
+// ArrayUpper returns the upper bound of the requested array dimension.
+//
+//	array_upper ( anyarray, integer ) → integer
+func ArrayUpper(arr builder.Exp, dim builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_upper", []builder.Exp{arr, dim})
+}
+
+// ArrayRemove removes all occurrences of the given value from the array.
+//
+//	array_remove ( anyarray, anyelement ) → anyarray
+func ArrayRemove(arr builder.Exp, elem builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_remove", []builder.Exp{arr, elem})
+}
+
+// ArrayReplace replaces each array element equal to the second argument with the third argument.
+//
+//	array_replace ( anyarray, anyelement, anyelement ) → anyarray
+func ArrayReplace(arr builder.Exp, from builder.Exp, to builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_replace", []builder.Exp{arr, from, to})
+}
+
+// ArrayPosition returns the subscript of the first occurrence of the second argument in the array, or NULL if it's not present.
+//
+//	array_position ( anyarray, anyelement [, integer ] ) → integer
+func ArrayPosition(arr builder.Exp, elem builder.Exp, start ...builder.Exp) builder.ExpBase {
+	args := []builder.Exp{arr, elem}
+	if len(start) > 0 {
+		args = append(args, start[0])
+	}
+	return builder.FuncExp("array_position", args)
+}
+
+// ArrayPositions returns an array of the subscripts of all occurrences of the second argument in the array given as first argument.
+//
+//	array_positions ( anyarray, anyelement ) → integer[]
+func ArrayPositions(arr builder.Exp, elem builder.Exp) builder.ExpBase {
+	return builder.FuncExp("array_positions", []builder.Exp{arr, elem})
+}
+
+// ArrayToString concatenates array elements using the supplied delimiter and optional null string.
+//
+//	array_to_string ( anyarray, text [, text ] ) → text
+func ArrayToString(arr builder.Exp, delim builder.Exp, nullString ...builder.Exp) builder.ExpBase {
+	args := []builder.Exp{arr, delim}
+	if len(nullString) > 0 {
+		args = append(args, nullString[0])
+	}
+	return builder.FuncExp("array_to_string", args)
+}
+
+// StringToArray splits string into array elements using supplied delimiter and optional null string.
+//
+//	string_to_array ( text, text [, text ] ) → text[]
+func StringToArray(text builder.Exp, delim builder.Exp, nullString ...builder.Exp) builder.ExpBase {
+	args := []builder.Exp{text, delim}
+	if len(nullString) > 0 {
+		args = append(args, nullString[0])
+	}
+	return builder.FuncExp("string_to_array", args)
+}
+
+// ArrayFill returns an array filled with copies of the given value, having dimensions of the lengths specified by the second argument.
+//
+//	array_fill ( anyelement, integer[] [, integer[] ] ) → anyarray
+func ArrayFill(value builder.Exp, dims builder.Exp, lowerBounds ...builder.Exp) builder.ExpBase {
+	args := []builder.Exp{value, dims}
+	if len(lowerBounds) > 0 {
+		args = append(args, lowerBounds[0])
+	}
+	return builder.FuncExp("array_fill", args)
+}
--- fn/array_test.go
+++ fn/array_test.go
@@ -24,4 +24,183 @@ func TestArrayFunctions(t *testing.T) {
 		SELECT * FROM unnest(ARRAY[1,2], ARRAY['foo','bar','baz']) AS x (a,b)
 		`, sql)
 	})
+
+	t.Run("array_append", func(t *testing.T) {
+		q := Select(fn.ArrayAppend(Array(Int(1), Int(2)), Int(3)))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_append(ARRAY[1,2], 3)
+		`, sql)
+	})
+
+	t.Run("array_prepend", func(t *testing.T) {
+		q := Select(fn.ArrayPrepend(Int(1), Array(Int(2), Int(3))))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_prepend(1, ARRAY[2,3])
+		`, sql)
+	})
+
+	t.Run("array_cat", func(t *testing.T) {
+		q := Select(fn.ArrayCat(Array(Int(1), Int(2)), Array(Int(3), Int(4))))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_cat(ARRAY[1,2], ARRAY[3,4])
+		`, sql)
+	})
+
+	t.Run("array_dims", func(t *testing.T) {
+		q := Select(fn.ArrayDims(Array(Int(1), Int(2), Int(3))))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_dims(ARRAY[1,2,3])
+		`, sql)
+	})
+
+	t.Run("array_ndims", func(t *testing.T) {
+		q := Select(fn.ArrayNdims(Array(Int(1), Int(2), Int(3))))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_ndims(ARRAY[1,2,3])
+		`, sql)
+	})
+
+	t.Run("array_length", func(t *testing.T) {
+		q := Select(fn.ArrayLength(Array(Int(1), Int(2), Int(3)), Int(1)))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_length(ARRAY[1,2,3], 1)
+		`, sql)
+	})
+
+	t.Run("array_lower", func(t *testing.T) {
+		q := Select(fn.ArrayLower(Array(Int(1), Int(2), Int(3)), Int(1)))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_lower(ARRAY[1,2,3], 1)
+		`, sql)
+	})
+
+	t.Run("array_upper", func(t *testing.T) {
+		q := Select(fn.ArrayUpper(Array(Int(1), Int(2), Int(3)), Int(1)))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_upper(ARRAY[1,2,3], 1)
+		`, sql)
+	})
+
+	t.Run("array_remove", func(t *testing.T) {
+		q := Select(fn.ArrayRemove(Array(Int(1), Int(2), Int(3), Int(2)), Int(2)))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_remove(ARRAY[1,2,3,2], 2)
+		`, sql)
+	})
+
+	t.Run("array_replace", func(t *testing.T) {
+		q := Select(fn.ArrayReplace(Array(Int(1), Int(2), Int(3)), Int(2), Int(99)))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_replace(ARRAY[1,2,3], 2, 99)
+		`, sql)
+	})
+
+	t.Run("array_position", func(t *testing.T) {
+		t.Run("without start", func(t *testing.T) {
+			q := Select(fn.ArrayPosition(Array(String("a"), String("b"), String("c")), String("b")))
+
+			sql, _, _ := Build(q).ToSQL()
+			testhelper.AssertSQLEquals(t, `
+			SELECT array_position(ARRAY['a','b','c'], 'b')
+			`, sql)
+		})
+
+		t.Run("with start", func(t *testing.T) {
+			q := Select(fn.ArrayPosition(Array(String("a"), String("b"), String("c"), String("b")), String("b"), Int(3)))
+
+			sql, _, _ := Build(q).ToSQL()
+			testhelper.AssertSQLEquals(t, `
+			SELECT array_position(ARRAY['a','b','c','b'], 'b', 3)
+			`, sql)
+		})
+	})
+
+	t.Run("array_positions", func(t *testing.T) {
+		q := Select(fn.ArrayPositions(Array(String("a"), String("b"), String("c"), String("b")), String("b")))
+
+		sql, _, _ := Build(q).ToSQL()
+		testhelper.AssertSQLEquals(t, `
+		SELECT array_positions(ARRAY['a','b','c','b'], 'b')
+		`, sql)
+	})
+
+	t.Run("array_to_string", func(t *testing.T) {
+		t.Run("without null string", func(t *testing.T) {
+			q := Select(fn.ArrayToString(Array(String("a"), String("b"), String("c")), String(",")))
+
+			sql, _, _ := Build(q).ToSQL()
+			testhelper.AssertSQLEquals(t, `
+			SELECT array_to_string(ARRAY['a','b','c'], ',')
+			`, sql)
+		})
+
+		t.Run("with null string", func(t *testing.T) {
+			q := Select(fn.ArrayToString(Array(String("a"), String("b"), Null()), String(","), String("*")))
+
+			sql, _, _ := Build(q).ToSQL()
+			testhelper.AssertSQLEquals(t, `
+			SELECT array_to_string(ARRAY['a','b',NULL], ',', '*')
+			`, sql)
+		})
+	})
+
+	t.Run("string_to_array", func(t *testing.T) {
+		t.Run("without null string", func(t *testing.T) {
+			q := Select(fn.StringToArray(String("a,b,c"), String(",")))
+
+			sql, _, _ := Build(q).ToSQL()
+			testhelper.AssertSQLEquals(t, `
+			SELECT string_to_array('a,b,c', ',')
+			`, sql)
+		})
+
+		t.Run("with null string", func(t *testing.T) {
+			q := Select(fn.StringToArray(String("a,b,*"), String(","), String("*")))
+
+			sql, _, _ := Build(q).ToSQL()
+			testhelper.AssertSQLEquals(t, `
+			SELECT string_to_array('a,b,*', ',', '*')
+			`, sql)
+		})
+	})
+
+	t.Run("array_fill", func(t *testing.T) {
+		t.Run("without lower bounds", func(t *testing.T) {
+			q := Select(fn.ArrayFill(String("x"), Array(Int(3))))
+
+			sql, _, _ := Build(q).ToSQL()
+			testhelper.AssertSQLEquals(t, `
+			SELECT array_fill('x', ARRAY[3])
+			`, sql)
+		})
+
+		t.Run("with lower bounds", func(t *testing.T) {
+			q := Select(fn.ArrayFill(String("x"), Array(Int(3), Int(2)), Array(Int(2), Int(5))))
+
+			sql, _, _ := Build(q).ToSQL()
+			testhelper.AssertSQLEquals(t, `
+			SELECT array_fill('x', ARRAY[3,2], ARRAY[2,5])
+			`, sql)
+		})
+	})
 }
